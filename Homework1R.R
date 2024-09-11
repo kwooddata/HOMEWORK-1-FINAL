02a007e42b50699e0140578a166d497f0e74ec95
kwooddata <kwood@hamilton.edu>
2024-09-11 17:33:40 +0000

    Homework1

---
+++ Homework1R.R
@@ -0,0 +1,6 @@
+clear all
+
+snowdepthininches <-c(2.5, 3, 5, 4.5)
+snowdepthincm <- snowdepthininches*2.54
+snowdepthincm
+exampleData <- data.frame(MountainName=c("Mount Haystack", "Mount Skylight", "Dix Mountian", "Gray Peak"", Elevationinfeet=d (4960, 4926, 4857, 4840), climeascentft=e(3570,4265,2800,4178) roundtriplengthmi=f(17.8,17.9,13.2,16))
