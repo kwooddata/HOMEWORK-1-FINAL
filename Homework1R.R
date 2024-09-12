4f24e1a67f97cc52b8c688d8e8b171267ee5c8ff
kwooddata <kwood@hamilton.edu>
2024-09-12 16:02:46 +0000

    Homework 1

--- Homework1R.R
+++ Homework1R.R
@@ -1,4 +1,4 @@
-#Start of activity and homework script
+#Start of activity 1 and in class work
 
 6^6
 
@@ -32,16 +32,34 @@ highPeaks <- data.frame(elev = peaks,
                         prom = prom,
                         name = peakNames)
 
-highPeaks [1;1]
+highPeaks [1,1]
 highPeaks [1,2]
 
-mean()
+####Prompt 3 classwork
+44*(9/5)+32
+0*(9/5)+32
+20*(9/5)+32
+35*(9/5)+32
+
+mean(peaks)
 ###################Start HOMEWORK 1#################
 snowdepthininches <-c(2.5, 3, 5, 4.5)
 snowdepthincm <- snowdepthininches*2.54
 snowdepthincm
-exampleData <- data.frame(
-MountainName=c("Mount Haystack", "Mount Skylight", "Dix Mountian", "Gray Peak"), 
+
+MountainName <- data.frame(Name = c("Mount Haystack", "Mount Skylight", "Dix Mountian", "Gray Peak"),
               Elevationinfeet=c(4960, 4926, 4857, 4840), 
               climeascentft=c(3570,4265,2800,4178),
               roundtriplengthmi=c(17.8,17.9,13.2,16))
+##Mountain peaks
+MountainName
+
+###Question 1
+Elevationinfeet=c(4960, 4926, 4857, 4840)
+Elevationinmeters<-Elevationinfeet/3.28
+Elevationinmeters
+
+###Question 3
+roundtriplengthmi=c(17.8,17.9,13.2,16)
+maxtrip <- max(roundtriplengthmi)
+maxtrip
